bed0f2dd082fc05f6bfd0c3ebb3b8e8883e68952
Christopher Cliff <chris@percolate.com>
2015-01-09 17:07:03 -0600

    Latest dependencies.

--- build.js
+++ build.js
@@ -5,16 +5,17 @@ var ignore = require('metalsmith-ignore')
 var markdown = require('metalsmith-markdown')
 var Metalsmith = require('metalsmith')
 var path = require('path')
+var serveStatic = require('serve-static')
 var templates = require('metalsmith-templates')
-var watch = require('metalsmith-watch')
+var watch = require('metalsmith-simplewatch')
 
 var isDev = false
 
-process.argv.forEach(function(val, index, array) {
+process.argv.forEach(function (val) {
     if (val === 'dev') isDev = true
 })
 
-var metalsmith = Metalsmith(__dirname)
+var metalsmith = new Metalsmith(__dirname)
 
 metalsmith
     .use(less({
@@ -24,44 +25,41 @@ metalsmith
         },
         render: {
             ieCompat: false,
-            compress: false
+            compress: false,
         },
-        useDefaultSourceMap: true
+        useDefaultSourceMap: true,
     }))
     .use(fingerprint({
-        pattern: 'css/index.css'
+        pattern: 'css/index.css',
     }))
     .use(markdown({
-        gfm: true
+        gfm: true,
     }))
     .use(templates({
         engine: 'handlebars',
         directory: './src/templates/',
-        pattern: '**/*.html'
+        pattern: '**/*.html',
     }))
     .use(ignore([
         'css/index.css',
         'fonts/*',
         'less/*',
-        'templates/*'
+        'templates/*',
     ]))
 
 if (isDev) {
-    metalsmith
-        .use(watch('**/*.less'))
-        .use(watch('**/*.hbs'))
-        .use(watch('**/*.md'))
+    metalsmith.use(watch())
 }
 
 metalsmith
-    .build(function(err){
+    .build(function (err) {
         if (err) throw err
     })
 
 if (isDev) {
     connect()
-        .use(connect.static(path.resolve(__dirname, './build/')))
-        .listen(8000, function(){
+        .use(serveStatic(path.resolve(__dirname, './build/')))
+        .listen(8000, function () {
             console.log('running on port 8000')
         })
 }
